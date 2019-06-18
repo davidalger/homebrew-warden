584c8475ea5913e4295729c62011ee027b004cd7
David Alger <davidmalger@gmail.com>
2019-06-18 10:29:37 -0500

    Published warden-0.1.0-beta4

--- Formula/warden.rb
+++ Formula/warden.rb
@@ -2,9 +2,9 @@
 class Warden < Formula
   desc "Warden is a CLI utility for working with docker-compose environments"
   homepage "https://warden.dev"
-  version "0.1.0-beta3"
-  url "https://github.com/davidalger/warden/archive/0.1.0-beta3.tar.gz"
-  sha256 "8a9ef1ab6eb0b62e8fec0fe74dbad46af57bdc9b7e543fcd62b1dce8eee456af"
+  version "0.1.0-beta4"
+  url "https://github.com/davidalger/warden/archive/0.1.0-beta4.tar.gz"
+  sha256 "05df1543b157511253d5288ba906b485f4a2433dc39cca4c3fe072627adc1eb8"
   head "https://github.com/davidalger/warden.git", :branch => "develop"
 
   def install
