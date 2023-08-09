1988ca6c669c1a95fed3ce34cc8c9e7a398ad601
Navarr Barnier <me@navarr.me>
2023-08-09 12:06:23 -0400

    Mirror `wardenenv/homebrew-warden`

--- Formula/warden.rb
+++ Formula/warden.rb
@@ -1,17 +1,38 @@
 
 class Warden < Formula
-  desc "Warden is a CLI utility for working with docker-compose environments.  Warden has moved to the wardenenv namespace"
+  desc "Warden is a CLI utility for working with docker-compose environments"
   homepage "https://warden.dev"
-  version "0.13.0"
-  url "file:///dev/null"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  license "MIT"
+  version "0.14.1"
+  url "https://github.com/wardenenv/warden/archive/0.14.1.tar.gz"
+  sha256 "c2f4f4485619ca291c296be5607a7ebfdab44caea3680529bf6685e3689717de"
+  head "https://github.com/wardenenv/warden.git", :branch => "main"
 
-  depends_on "wardenenv/warden/warden"
-  
+  depends_on "gettext"
+  depends_on "pv" => :recommended
+
   def install
-    (bin+"wardenInstall").write <<-EOS
-        #!/bin/sh
-        echo "Warden Installed"
+    prefix.install Dir["*"]
+  end
+
+  def caveats
+    <<~EOS
+      Warden manages a set of global services on the docker host machine. You
+      will need to have Docker installed and docker-compose available in your
+      local $PATH configuration prior to starting Warden.
+
+      To start warden simply run:
+
+        warden svc up
+
+      This command will automatically run "warden install" to setup a trusted
+      local root certificate and sign an SSL certificate for use by services
+      managed by warden via the "warden sign-certificate warden.test" command.
+
+      To print a complete list of available commands simply run "warden" without
+      any arguments.
+
+      Documentation is available at: https://docs.warden.dev
     EOS
   end
 end
